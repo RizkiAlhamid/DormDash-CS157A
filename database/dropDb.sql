53d1f02f37dbdc1e1ff49a841511faa6fd307415
Rizki Miftha <riskimifta@gmail.com>
2024-05-09 17:12:47 -0700

    added property type to the database

--- database/dropDb.sql
+++ database/dropDb.sql
@@ -6,6 +6,7 @@ DROP TABLE IF EXISTS PropertiesPhotos;
 DROP TABLE IF EXISTS Properties;
 DROP TABLE IF EXISTS Users;
 DROP TABLE IF EXISTS Schools;
+DROP TABLE IF EXISTS PropertyType;
 
 -- Drop the database itself
 DROP DATABASE IF EXISTS myDB;
